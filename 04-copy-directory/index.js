b7fd9e40159c5e53fbe026abf03bfaf90d8a4106
Pavel Shlapak <letohx@gmail.com>
2024-01-23 22:18:33 +0300

    fix: solution 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,28 +4,35 @@ const pathFolder = path.join(__dirname, 'files');
 const pathFolderCopy = path.join(__dirname, 'files-copy');
 
 (function copyDir() {
-    fs.mkdir(pathFolderCopy, { recursive: true }, (error) => {
+    fs.rm(pathFolderCopy, { recursive: true, force: true }, (error) => {
         if (error) {
-            console.log('Folder not created', error.message);
+            console.log('Folder not del', error.message);
             return;
         }
 
-        fs.readdir(pathFolder, { withFileTypes: true }, (error, files) => {
+        fs.mkdir(pathFolderCopy, { recursive: true }, (error) => {
             if (error) {
-                console.log('Folder unread', error.message);
+                console.log('Folder not created', error.message);
                 return;
             }
 
-            files.forEach((file) => {
-                const source = path.join(pathFolder, file.name);
-                const purpose = path.join(pathFolderCopy, file.name);
-                fs.copyFile(source, purpose, error => {
-                    if (error) {
-                        console.log('Error copying', error.message);
-                        return;
-                    }
-                });
-            })
+            fs.readdir(pathFolder, { withFileTypes: true }, (error, files) => {
+                if (error) {
+                    console.log('Folder unread', error.message);
+                    return;
+                }
+
+                files.forEach((file) => {
+                    const source = path.join(pathFolder, file.name);
+                    const purpose = path.join(pathFolderCopy, file.name);
+                    fs.copyFile(source, purpose, error => {
+                        if (error) {
+                            console.log('Error copying', error.message);
+                            return;
+                        }
+                    });
+                })
+            });
         });
     });
 })();
